6207820ca88d97d6a052f1eb2f43ffdd8dc40776
NamPE286 <nambuihung654@gmail.com>
2022-08-19 05:35:45 +0700

    update

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const supabase = require('@supabase/supabase-js').createClient(process.env.API_U
 var userData;
 
 function isAdmin(token){
-    console.log(process.env.DEVELOPMENT_SERVER)
-    if(process.env.DEVELOPMENT_SERVER) return true
+    //if(process.env.DEVELOPMENT_SERVER) return true
     try{
         jwt.verify(token, process.env.JWT_SECRET)
         async function getData(){
